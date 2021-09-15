22cbed75c5a38245b650337da60c6795fed14ba7
김인수 <kim-insu@gim-insuui-MacBookPro.local>
2021-09-15 14:57:16 +0900

    수정

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,2 +1,31 @@
-package hello.springmvc.basic;public class LogTestController {
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Slf4j
+@RestController //String이 그대로 반환이 된다. "ok"
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass()); //getClass() = LogTestController.class
+// Slf4j가 위에있는 Logger log를 대신해준다.
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name =   "Spring";
+
+        System.out.println("name = " + name);
+
+
+       // log.info(" info log=" + name);  //이렇게 사용하면 안된다.
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);  //개발사 같은 데서 보는
+        log.info("info log={}", name);    //중요한 정보를 보는 것
+        log.warn("warn log={}", name);    //위험한 것
+        log.error("error log={}", name);  //에러
+        return "ok";
+    }
+
 }
--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -1,2 +1,31 @@
-package hello.springmvc.basic.response;public class ResponseViewController {
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello").addObject("data", "hello!");
+
+        return mav;
+    }
+
+    // @ResponseBody 를 사용하면 ViewResolver 를 실행하지않고 HTTP Message Body에 response/hello 가 그대로 보여 버린다.
+    // @RestController도 마찬가지
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello"; //@ Controller 면서 String으로 반환하면 return 값이 논리적 뷰의 이름이 된다.
+    }
+
+    //추천하지 않는다. 불명확해서
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
 }
